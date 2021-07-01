3be3f2a2310111dfbc66a10e1ec1433b46f95610
VaibhavMR10 <78018950+VaibhavMR10@users.noreply.github.com>
2021-07-02 00:18:27 +0530

    Create Background.js

---
+++ Background.js
@@ -0,0 +1,8 @@
+Background.js
+
+a. Service Workers
+            Extensions are event-based programs used to modify or enhance the Chrome browsing experience. 
+            Events are browser triggers, such as navigating to a new page, removing a bookmark, or closing a tab. 
+            Extensions monitor these events using scripts in their background service worker, which then react with specified instructions.
+    
+            A background service worker is loaded when it is needed, and unloaded when it goes idle. Some examples of events include:
